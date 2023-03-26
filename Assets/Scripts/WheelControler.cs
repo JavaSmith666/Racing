2cda548d2bc16297e57ad57e03b9813107a15ebc
JavaSmith666 <1263204012@qq.com>
2023-03-27 00:42:53 +0800

    the car can drift normally

--- Assets/Scripts/WheelControler.cs
+++ Assets/Scripts/WheelControler.cs
@@ -4,77 +4,153 @@ using UnityEngine;
 
 public class WheelControler : MonoBehaviour
 {
-    public float maxAngle = 35f;
-    public float angleSpeed = 50f;
-    public float moveSpeed = 200;
-    public float breakMove = (float)1e8;
-
-    public Transform LF_Wheel, RF_Wheel;
-
+    public float angleSpeed = 100f;
+    public float maxSpeed = 120f;
+    public Transform LF_Wheel, RF_Wheel, LB_Wheel, RB_Wheel;
     public WheelCollider LF_Collider, LB_Collider, RF_Collider, RB_Collider;
+    private float maxAngleInner = 39.6f;
+    private float maxAngleOuter = 33.5f;
+    private float driftThreshold = 30f;
+    private float horizontalForceDrift;
+    private float motorTorque;
+    private float brakeTorque;
+    private int velocity;
+    private Vector3 lastPos;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        lastPos = gameObject.transform.position;
+        Rigidbody carRigidBody = gameObject.GetComponent<Rigidbody>();
+        horizontalForceDrift = carRigidBody.mass * 5f;
+        motorTorque = carRigidBody.mass * 4f;
+        brakeTorque = carRigidBody.mass * 4f;
+        carRigidBody.centerOfMass = Vector3.zero;
+    }
+
     private void FixedUpdate()
     {
-        float speed = moveSpeed;
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = moveSpeed * 1.5f;
-        }
-        //³µÂÖÅ¤¾Ø
-        if (Input.GetKey(KeyCode.W))
-        {
-            LB_Collider.motorTorque = RB_Collider.motorTorque = speed;
-            LB_Collider.brakeTorque = RB_Collider.brakeTorque = .0f;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            LB_Collider.motorTorque = RB_Collider.motorTorque = -speed;
-            LB_Collider.brakeTorque = RB_Collider.brakeTorque = .0f;
-        }
-        else
-        {
-            LB_Collider.motorTorque = RB_Collider.motorTorque = .0f;
-            LB_Collider.brakeTorque = RB_Collider.brakeTorque = breakMove;
-        }
+        //Set torque of back wheels.
+        WheelsTorqueUpdate();
+        WheelsAngleUpdate();
 
-        //ÂÖÌ¥Æ«×ª
-        if (Input.GetKey(KeyCode.A))
+        //Update the position of four wheels.
+        WheelsModelUpdate(LF_Wheel, LF_Collider);
+        WheelsModelUpdate(RF_Wheel, RF_Collider);
+        WheelsModelUpdate(LB_Wheel, LB_Collider);
+        WheelsModelUpdate(RB_Wheel, RB_Collider);
+
+        //Velocity of car.
+        velocity = (int)((transform.position - lastPos).magnitude / Time.fixedDeltaTime * 3.6f);
+        Debug.LogFormat("current velocity: {0} km/h", velocity);
+
+        //Update the position of lastPos at the end of function FixedUpdate.
+        lastPos = transform.position;
+    }
+
+    //Update the torque of wheels.
+    private void WheelsTorqueUpdate()
+    {
+        //Torque of wheels.
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            && !(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)))
         {
-            LF_Collider.steerAngle -= Time.deltaTime * angleSpeed;
-            RF_Collider.steerAngle -= Time.deltaTime * angleSpeed;
-            if (LF_Collider.steerAngle < -maxAngle || RF_Collider.steerAngle < -maxAngle)
-            {
-                LF_Collider.steerAngle = RF_Collider.steerAngle = -maxAngle;
-            }
+            DoTorqueUpdate(transform.position - lastPos);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            && !(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)))
         {
-            LF_Collider.steerAngle += Time.deltaTime * angleSpeed;
-            RF_Collider.steerAngle += Time.deltaTime * angleSpeed;
-            if (LF_Collider.steerAngle > maxAngle || RF_Collider.steerAngle > maxAngle)
-            {
-                LF_Collider.steerAngle = RF_Collider.steerAngle = maxAngle;
-            }
+            DoTorqueUpdate(transform.position - lastPos);
         }
         else
         {
-            LF_Collider.steerAngle = RF_Collider.steerAngle = .0f;
+            LB_Collider.motorTorque = RB_Collider.motorTorque = 0f;
+            LF_Collider.brakeTorque = RF_Collider.brakeTorque = 0.1f * brakeTorque;
+            LB_Collider.brakeTorque = RB_Collider.brakeTorque = 0.1f * brakeTorque;
+        }
+        if (velocity >= maxSpeed)
+        {
+            LB_Collider.motorTorque = RB_Collider.motorTorque = 0f;
+            LF_Collider.brakeTorque = RF_Collider.brakeTorque = brakeTorque;
+            LB_Collider.brakeTorque = RB_Collider.brakeTorque = brakeTorque;
         }
     }
 
-    //Ç°ÂÖ×·Ëæ³µÂÖÅö×²Æ÷Î»ÖÃ¼°Æ«×ª½Ç¶È
-    private void WheelsModel_Update(Transform t, WheelCollider wheel)
+    private void AddHorizontalForce()
+    {
+        if(velocity >= driftThreshold)
+        {
+            Vector3 right = (transform.Find("Wheels").Find("RF_Wheel").position -
+            transform.Find("Wheels").Find("LF_Wheel").position).normalized;
+            Vector3 forcePos = transform.position + transform.forward * -1f;
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                gameObject.GetComponent<Rigidbody>().AddForceAtPosition(right * horizontalForceDrift, forcePos);
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                gameObject.GetComponent<Rigidbody>().AddForceAtPosition(-right * horizontalForceDrift, forcePos);
+            }
+        }
+    }
+
+    private void DoTorqueUpdate(Vector3 v_dir)
+    {
+        float fvVal = Vector3.Dot(transform.forward * Input.GetAxisRaw("Vertical"), v_dir);
+        if (fvVal < 0f && Mathf.Abs(fvVal) > 1e-6f)
+        {
+            //braking
+            AddHorizontalForce();
+            LB_Collider.motorTorque = RB_Collider.motorTorque = 0f;
+            LF_Collider.brakeTorque = RF_Collider.brakeTorque = brakeTorque;
+            LB_Collider.brakeTorque = RB_Collider.brakeTorque = brakeTorque;
+        }
+        else
+        {
+            //accelerating
+            LB_Collider.motorTorque = RB_Collider.motorTorque = Input.GetAxisRaw("Vertical") *  motorTorque;
+            LF_Collider.brakeTorque = RF_Collider.brakeTorque = 0f;
+            LB_Collider.brakeTorque = RB_Collider.brakeTorque = 0f;
+        }
+    }
+
+    private void WheelsAngleUpdate()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            LF_Collider.steerAngle -= Time.fixedDeltaTime * angleSpeed;
+            RF_Collider.steerAngle -= Time.fixedDeltaTime * angleSpeed;
+            if (LF_Collider.steerAngle < -maxAngleInner)
+            {
+                LF_Collider.steerAngle = -maxAngleInner;
+            }
+            if (RF_Collider.steerAngle < -maxAngleOuter)
+            {
+                RF_Collider.steerAngle = -maxAngleOuter;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            LF_Collider.steerAngle += Time.fixedDeltaTime * angleSpeed;
+            RF_Collider.steerAngle += Time.fixedDeltaTime * angleSpeed;
+            if (LF_Collider.steerAngle > maxAngleOuter)
+            {
+                LF_Collider.steerAngle = maxAngleOuter;
+            }
+            if (RF_Collider.steerAngle > maxAngleInner)
+            {
+                RF_Collider.steerAngle = maxAngleInner;
+            }
+        }
+        else
+        {
+            LF_Collider.steerAngle = RF_Collider.steerAngle = 0f;
+        }
+    }
+
+    //Wheel models follow the position and rotation of wheel colliders.
+    private void WheelsModelUpdate(Transform t, WheelCollider wheel)
     {
         wheel.GetWorldPose(out Vector3 pos, out Quaternion rot);
         t.SetPositionAndRotation(pos, rot);
-    }
-
-    private void Update()
-    {
-        WheelsModel_Update(LF_Wheel, LF_Collider);
-        WheelsModel_Update(RF_Wheel, RF_Collider);
-        float velocity = LF_Collider.rpm * 2 * Mathf.PI * LF_Collider.radius * 60 / 1000;
-        Debug.LogFormat("current velocity: {0} km/h", velocity);
     }
 }
